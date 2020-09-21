5b14875705ecaf59afd56d6aabef78726384d320
apokochito <diana.apolinar@hotmail.com>
2020-09-21 14:11:31 -0500

    Database Connection and environment variables added

---
+++ src/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const {MONGODB_USER, MONGODB_PASS, MONGODB_DB, MONGODB_CLUSTER} = process.env;
+const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@${MONGODB_CLUSTER}.pnk2h.mongodb.net/${MONGODB_DB}?retryWrites=true&w=majority`;
+
+mongoose.connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+.then(db => console.log('Database Connected'))
+.catch(err => console.log(err));
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+require('dotenv').config({path: '.env'});
 const app = require("./server");
+require('./database');
 
 app.listen(app.get('port'), () => {
     console.log("Hey man", app.get('port'))
