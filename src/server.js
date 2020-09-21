30b4e4c86e96f5fe17886b52b9867e6b60666ab9
apokochito <diana.apolinar@hotmail.com>
2020-09-21 16:53:11 -0500

    Handlebars added

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
+const handlebars = require('express-handlebars');
 
 // Initializations
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 4000);
-app.set('views', path.join(__dirname + 'views'));
+app.set('views', path.join(__dirname , '..', 'src', 'views'));
+app.engine('.hbs', handlebars({
+    defaultLayout: 'default',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(express.urlencoded({extended: false})); // Convert each received resource to json
@@ -15,7 +23,7 @@ app.use(express.urlencoded({extended: false})); // Convert each received resourc
 
 // Routes
 app.get('/', (request, response) => {
-    response.send('Wops');
+    response.render('index');
 })
 
 // Static Files
